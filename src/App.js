dee590ac2c70dd92034349b1f6fc68da183118c7
Felix Lugo <felixdlugo@gmail.com>
2021-11-09 14:26:51 -0400

    Added navigation to menu item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-const HatPage = () => (
+const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
   </div>
@@ -12,7 +13,8 @@ const HatPage = () => (
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/shop/hats' component={HatsPage}/>
     </div>
   );
 }
