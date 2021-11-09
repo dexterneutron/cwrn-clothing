e1ebf282fb5406e1c3c97144596d15bac83a3143
Felix Lugo <felixdlugo@gmail.com>
2021-11-09 12:03:21 -0400

    Fixed WithRouter issue

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
