9b20400beeff6a5ddb90c979932056d1da3d1e71
Felix Lugo <felixdlugo@gmail.com>
2021-11-10 17:46:39 -0400

    Added sign in form

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,12 +3,6 @@ import React from "react";
 import './homepage.styles.scss'
 import Directory from "../../components/directory/directory.component";
 
-const HatsPage = () =>(
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-);
-
 const HomePage = () =>(
     <div className='homepage'>
         <Directory/>
