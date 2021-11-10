898641118ca3908617ab52f62c1814a61282628d
Felix Lugo <felixdlugo@gmail.com>
2021-11-09 20:40:50 -0400

    Added styles to collection and collection preview

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './homepage.styles.scss'
+import Directory from "../../components/directory/directory.component";
+
+const HatsPage = () =>(
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+);
+
+const HomePage = () =>(
+    <div className='homepage'>
+        <Directory/>
+
+    </div>
+);
+
+export default HomePage;
+
