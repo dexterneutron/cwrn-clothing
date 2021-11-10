80e4fdacb597815e375bdf99bd686577d310c9cb
Felix Lugo <felixdlugo@gmail.com>
2021-11-10 09:33:03 -0400

    Styling shop

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -7,7 +7,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
         <div
         className='image'
         style={{
-            backgroundImage: `url:(${imageUrl})`
+            backgroundImage: `url(${imageUrl})`
         }}
         />
         <div className='collection-footer'>
@@ -18,3 +18,5 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
     </div>
 
 );
+
+export default CollectionItem
