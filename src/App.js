bc19791782d3357274fa8b850a514dad7a8367e9
Felix Lugo <felixdlugo@gmail.com>
2021-11-11 15:33:34 -0400

    Finished google auth

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.conponent';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component 
@@ -23,9 +23,18 @@ class App extends React.Component
 
   componentDidMount(){
     this.unsubscribeFromAuth = auth.onAuthStateChanged(
-      user=>{
-        this.setState({currentUser:user})
-        console.log(user);
+      async userAuth=>{
+        if (userAuth){
+          const userRef = await createUserProfileDocument(userAuth);
+
+          userRef.onSnapshot(snapShot=>{
+            this.setState({
+              id: snapShot.id,
+              ...snapShot.data()
+            });
+          });
+        }
+        this.setState({currentUser:userAuth});
       });
   }
 
