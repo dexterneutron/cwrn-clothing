f257f925028e25a5c8f69c6776d8f1652fcf1294
Felix Lugo <felixdlugo@gmail.com>
2021-11-15 19:23:00 -0400

    Added redux to logged user state

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import reportWebVitals from './reportWebVitals';
 
 import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux'
+import store from './redux/store'
 
 ReactDOM.render(
+<Provider store={store}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+</Provider>,
   document.getElementById('root')
 );
 
