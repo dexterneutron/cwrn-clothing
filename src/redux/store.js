c036efec48b7f5bd9fa46b503dfac28f4bc3056e
Felix Lugo <felixdlugo@gmail.com>
2021-11-24 08:51:19 -0400

    Implemented container patter for async retrieval of shop data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,19 @@
+
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import { persistStore } from 'redux-persist'
+import thunk from 'redux-thunk';
+
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [thunk];
 
-if (process.env.NODE_ENV === 'development'){
-    middlewares.push(logger)
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares) && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
+
+export default { store, persistStore };
